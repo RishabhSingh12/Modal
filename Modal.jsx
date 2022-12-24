fc0bc6a808fc13a123539e6469048f68302d96c0
RISHABH KUMAR <rishabh.kumar@gida.io>
2022-12-24 12:12:31 +0530

    Initial commit- Added stylings and structures for the modal

---
+++ Modal.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import "./Modal.css"
+
+const Modal = (props) => {
+
+    const { show, showmodal } = props;
+    const clickHandler = () => {
+        props.showmodal(false);
+    }
+
+    return (
+        <div className='modal-container'>
+            <div className='content'>
+                <p className='info-text'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas placeat magnam officia consectetur quod atque itaque rem reiciendis sapiente rerum quaerat, fugiat repudiandae provident est fugit voluptates exercitationem nostrum distinctio!
+                    Dolorem nam nulla natus, perferendis enim voluptatem rem nisi! Eum incidunt vitae in saepe ea asperiores inventore aliquam suscipit beatae earum. Corporis, aliquid molestias odio obcaecati laudantium non molestiae voluptatem.</p>
+                <button className=' close' onClick={clickHandler}>
+                    <p></p>
+                    <p></p>
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Modal
