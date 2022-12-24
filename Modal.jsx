278715bc7e5b3f40d5b5ecaba88aee6ce750f4c3
RISHABH KUMAR <rishabh.kumar@gida.io>
2022-12-24 12:13:42 +0530

    Modified stylings and structures for the modal and added animation for the initial visibility

--- Modal.jsx
+++ Modal.jsx
@@ -9,11 +9,11 @@ const Modal = (props) => {
     }
 
     return (
-        <div className='modal-container'>
+        show && <div className='modal-container'>
             <div className='content'>
                 <p className='info-text'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas placeat magnam officia consectetur quod atque itaque rem reiciendis sapiente rerum quaerat, fugiat repudiandae provident est fugit voluptates exercitationem nostrum distinctio!
                     Dolorem nam nulla natus, perferendis enim voluptatem rem nisi! Eum incidunt vitae in saepe ea asperiores inventore aliquam suscipit beatae earum. Corporis, aliquid molestias odio obcaecati laudantium non molestiae voluptatem.</p>
-                <button className=' close' onClick={clickHandler}>
+                <button className='close' onClick={clickHandler}>
                     <p></p>
                     <p></p>
                 </button>
